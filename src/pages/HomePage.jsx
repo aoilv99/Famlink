c9a78c8bf0485139a792236267da751e5c3c723d
aoilv99 <aoiiiiilv999@gmail.com>
2025-11-25 11:37:44 +0900

    refactor: ホーム画面のファイルの作成

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div style={{ padding: '40px', textAlign: 'center' }}>
+      <h1>ようこそ！</h1>
+      <p>これはログイン後のメイン画面です。</p>
+    </div>
+  );
+};
+
+export default HomePage;
