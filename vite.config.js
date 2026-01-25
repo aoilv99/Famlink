81dbde0fb6afcdbd7a959fdbdc22c7ae1aadfbad
aoilv99 <aoiiiiilv999@gmail.com>
2026-01-25 23:06:39 +0900

    Viteのホスト許可設定を追加

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['famlink-production.up.railway.app'], // RailwayのURLを許可するよ
+    host: true,
+    port: 5173
+  }
 })
